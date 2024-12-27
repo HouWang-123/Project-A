92c11c65feb38057a60e16b75a5e592dc9934484
S-JQ <941361174@qq.com>
2024-12-27 18:42:22 +0800

    资源加载，程序入口

---
+++ Assets/Scripts/GameRoot.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+using YooAsset;
+
+public class GameRoot : MonoBehaviour
+{
+    public EPlayMode PlayMode = EPlayMode.EditorSimulateMode;
+    ResourcePackage package;
+
+    private void Awake()
+    {
+        YooAssets.Initialize();
+        package = YooAssets.CreatePackage("DefaultPackage");
+
+        YooAssets.SetDefaultPackage(package);
+
+        switch(PlayMode)
+        {
+            case EPlayMode.EditorSimulateMode:
+                StartCoroutine(EditorInitPackage());
+                break;
+            case EPlayMode.OfflinePlayMode:
+                StartCoroutine(OfflineInitPackage());
+                break;
+            case EPlayMode.HostPlayMode:
+                break;
+            case EPlayMode.WebPlayMode:
+                break;
+        }
+    }
+
+    private IEnumerator EditorInitPackage()
+    {
+        var initParameters = new EditorSimulateModeParameters();
+        var simulateManifestFilePath = EditorSimulateModeHelper.SimulateBuild(EDefaultBuildPipeline.BuiltinBuildPipeline, "DefaultPackage");
+        initParameters.SimulateManifestFilePath = simulateManifestFilePath;
+        yield return package.InitializeAsync(initParameters);
+        yield return null;
+        GameStart();
+    }
+
+    private IEnumerator OfflineInitPackage()
+    {
+        var initParameters = new OfflinePlayModeParameters();
+        yield return package.InitializeAsync(initParameters);
+        yield return null;
+        GameStart();
+    }
+
+
+    private void GameStart()
+    {
+        //Data
+        AssetHandle handle2 = YooAssets.LoadAssetSync<GameObject>("GameObject");
+        GameObject @object = Instantiate(handle2.AssetObject) as GameObject;
+        
+    }
+}
