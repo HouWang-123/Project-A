8c5b5ed64c8688efad9ae4f9de1f416d6932eee3
S-JQ <941361174@qq.com>
2025-02-11 15:28:16 +0800

    资源位置更新

--- Assets/Scripts/GameScripts/BulletControl.cs
+++ Assets/Scripts/GameScripts/BulletControl.cs
@@ -6,6 +6,7 @@ public class BulletControl : MonoBehaviour
 {
     public float Speed = 10;
 
+
     private void FixedUpdate()
     {
         transform.position += transform.right * Speed * Time.deltaTime;
