aadf769f1bb0d2cf0e6374155e2d377ac47a00fa
AlanFEVM <fevm28017148971@gmail.com>
2025-02-28 19:05:27 +0800

    [ADD] 添加根据世界坐标添加物品的方法

--- Assets/Scripts/GameScripts/GameTool.cs
+++ Assets/Scripts/GameScripts/GameTool.cs
@@ -1,12 +1,14 @@
 using System;
+using System.Numerics;
 using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
 using YooAsset;
+using Vector3 = UnityEngine.Vector3;
 
 public static class GameTool
 {
-    public static void GenerateItemAtTransform(int id, Transform parent, Action<ItemBase> res = null)
+    private static string GetPrefabName(int id)
     {
         string uri = "";
         if (id.ToString().StartsWith("20")) //FOOD
@@ -39,6 +41,11 @@ public static class GameTool
             var weapon = GameTableDataAgent.WeaponTable.Get(id);
             uri = weapon.PrefabName;
         }
+        return uri;
+    }
+    public static void GenerateItemAtTransform(int id, Transform parent, Action<ItemBase> res = null)
+    {
+        string uri = GetPrefabName(id);
         if (uri != "")
         {
             ItemBase ib;
@@ -49,10 +56,30 @@ public static class GameTool
                 {
                     GameObject instantiate = GameObject.Instantiate(loadAssetAsync.AssetObject, parent) as GameObject;
                     instantiate.transform.SetParent(GameControl.Instance.GetSceneItemList().transform);
-                    ib = instantiate.GetComponent<ItemBase>();
-                    ib.OnItemDrop(false);
-                    ib.SetItemId(id);
-                    res?.Invoke(ib);
+                    ib = instantiate.GetComponent<ItemBase>(); ib.OnItemDrop(false); ib.SetItemId(id); res?.Invoke(ib);
+                };
+            }
+            else
+            {
+                Debug.LogWarning("预制体 " + uri + " 没有找到");
+            }
+        }
+    }
+    public static void GenerateItemAtTransform(int id, Vector3 worldPos, Action<ItemBase> res = null)
+    {
+        string uri = GetPrefabName(id);
+        if (uri != "")
+        {
+            ItemBase ib;
+            if (YooAssets.CheckLocationValid(uri))
+            {
+                AssetHandle loadAssetAsync = YooAssets.LoadAssetAsync<GameObject>(uri);
+                loadAssetAsync.Completed += handle =>
+                {
+                    GameObject instantiate = GameObject.Instantiate(loadAssetAsync.AssetObject) as GameObject;
+                    instantiate.transform.SetParent(GameControl.Instance.GetSceneItemList().transform);
+                    instantiate.transform.position = worldPos; ib = instantiate.GetComponent<ItemBase>();
+                    ib.OnItemDrop(false); ib.SetItemId(id); res?.Invoke(ib);
                 };
             }
             else
