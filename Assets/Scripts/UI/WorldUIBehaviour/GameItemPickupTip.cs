29f9dcad17874b81bb0558872088ba33695791ff
AlanFEVM <fevm28017148971@gmail.com>
2025-04-23 10:32:08 +0800

    问题提交

--- Assets/Scripts/UI/WorldUIBehaviour/GameItemPickupTip.cs
+++ Assets/Scripts/UI/WorldUIBehaviour/GameItemPickupTip.cs
@@ -6,7 +6,7 @@ public class GameItemPickupTip : MonoBehaviour
 {
     public void PlayInitAnimation()
     {
-        DOTween.Kill(this);
+        DOTween.Kill(gameObject);
         transform.localScale = Vector3.zero;
             transform.DOScale(1f, 0.2f);
         
@@ -14,13 +14,13 @@ public class GameItemPickupTip : MonoBehaviour
 
     public void OnDetargeted()
     {
-        DOTween.Kill(this);
+        DOTween.Kill(gameObject);
         transform.DOScale(0f, 0.2f);
     }
 
     public void OnDestroy()
     {
-        DOTween.Kill(this);
+        DOTween.Kill(gameObject);
     }
 
     public void OnItemPicked()
