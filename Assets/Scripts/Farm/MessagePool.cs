1a0ba0df64d209498fde7d5ac20369bf587fc710
mkljklhiklu <980047487@qq.com>
2025-04-23 16:35:40 +0800

    消息池添加

---
+++ Assets/Scripts/Farm/MessagePool.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+public class MessagePool : SingleDataMgr<MessagePool>
+{
+    private Dictionary<int, Queue<string>> messageDict = new Dictionary<int, Queue<string>>();
+    
+    public void SendMessageTo(int targetId, string message)
+    {
+        if (!messageDict.ContainsKey(targetId))
+        {
+            messageDict[targetId] = new Queue<string>();
+        }
+        messageDict[targetId].Enqueue(message);
+    }
+
+    public List<string> RetrieveMessages(int receiverId)
+    {
+        if (!messageDict.ContainsKey(receiverId))
+            return new List<string>();
+
+        var messages = new List<string>(messageDict[receiverId]);
+        messageDict[receiverId].Clear();
+        return messages;
+    }
+
+    public bool HasMessages(int receiverId)
+    {
+        return messageDict.ContainsKey(receiverId) && messageDict[receiverId].Count > 0;
+    }
+}
