55dd68c51729af65201e3949a099380feaf4e80a
mkljklhiklu <980047487@qq.com>
2025-04-25 16:00:01 +0800

    添加光控开关

---
+++ Assets/Scripts/GameScripts/Item/InterfacesAndBassClasses/SceneItems/LightSWMono.cs
@@ -0,0 +1,43 @@
+using System;
+using UnityEngine;
+
+public class LightSWMono : BaseSWMono
+{
+    public override void ChangeState()
+    {
+        base.ChangeState();
+    }
+
+    public override void OnPlayerFocus()
+    {
+        base.OnPlayerFocus();
+    }
+
+    public override void OnPlayerDefocus()
+    {
+        base.OnPlayerDefocus();
+    }
+
+    public override void OnPlayerStartInteract()
+    {
+        base.OnPlayerStartInteract();
+    }
+
+    public override void OnPlayerInteractCancel()
+    {
+        base.OnPlayerInteractCancel();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //todo 射线触发开关
+        IsOn = true;
+        ChangeState();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IsOn = false;
+        ChangeState();
+    }
+}
